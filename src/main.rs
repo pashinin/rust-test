5d365e82c7f42abe67b2f0c0b637a0f3290e4ff6
Sergey Pashinin <sergey@pashinin.com>
2016-10-12 01:49:40 +0300

    window app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+#[macro_use]
+extern crate glium;
+
 fn main() {
-    println!("Hello World!");
+    use glium::{DisplayBuild, Surface};
+    let display = glium::glutin::WindowBuilder::new()
+        .with_title(format!("Rusted"))
+        .build_glium().unwrap();
+
+    loop {
+        let mut target = display.draw();
+        // 002a35   42 53
+        target.clear_color(0.0, 0.16, 0.2, 1.0);
+        target.finish().unwrap();
+
+        for ev in display.poll_events() {
+            match ev {
+                glium::glutin::Event::Closed => return,
+                _ => ()
+            }
+        }
+    }
 }

6af617e021a794f7dae4d36a9289a036a8419ac4
Sergey Pashinin <sergey@pashinin.com>
2016-10-12 14:12:06 +0300

    wait_events

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
 #[macro_use]
 extern crate glium;
+use glium::glutin::{Event, VirtualKeyCode, WindowBuilder};
+use glium::backend::glutin_backend::GlutinFacade;
+
+
+fn draw(display:&GlutinFacade){
+    use glium::{Surface};
+    let mut target = display.draw();
+    target.clear_color(0.0, 0.16, 0.2, 1.0);
+    target.finish().unwrap();
+}
 
 fn main() {
-    use glium::{DisplayBuild, Surface};
-    let display = glium::glutin::WindowBuilder::new()
+    use glium::{DisplayBuild};
+    let display = WindowBuilder::new()
         .with_title(format!("Rusted"))
         .build_glium().unwrap();
 
     loop {
-        let mut target = display.draw();
-        // 002a35   42 53
-        target.clear_color(0.0, 0.16, 0.2, 1.0);
-        target.finish().unwrap();
+        // for ev in display.poll_events() {   // slower
+        for ev in display.wait_events() {
+            // display.get_window().swap_buffers();
 
-        for ev in display.poll_events() {
             match ev {
                 glium::glutin::Event::Closed => return,
+                // Quit on Esc:
+                Event::KeyboardInput(_ , _, Some(VirtualKeyCode::Escape)) => return,
+                Event::Refresh => {
+                    draw(&display);
+                },
                 _ => ()
             }
         }
